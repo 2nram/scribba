2dec9c48b539417788d563c77694bd32ea4ece65
2nram <dosenram@gmail.com>
2018-06-12 20:17:54 -0500

    Version 0.0.1 Activities
    Se elaboraron los diferentes activities junto los los layout de registro y welcome

--- app/src/main/java/com/a2nram/scribba/WelcomeActivity.java
+++ app/src/main/java/com/a2nram/scribba/WelcomeActivity.java
@@ -1,13 +1,39 @@
 package com.a2nram.scribba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+        btnSignIn = (Button) findViewById(R.id.buttonSignIn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogin = new Intent(WelcomeActivity.this , LoginActivity.class);
+                startActivity(intentLogin);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSignIn = new Intent(WelcomeActivity.this , LoginActivity.class);
+                startActivity(intentSignIn);
+            }
+        });
+
     }
 }
