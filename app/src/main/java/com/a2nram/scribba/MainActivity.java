bde8dfdc46f73f79c7a7f251e71ab839d9b8f17f
2nram <dosenram@gmail.com>
2018-06-26 20:33:44 -0500

    Versión 0.0.4 Icono Hamburguesa
    se creo el icono del menú de hamburguesa junto con la función que lo habilita dentro del toolbar

--- app/src/main/java/com/a2nram/scribba/MainActivity.java
+++ app/src/main/java/com/a2nram/scribba/MainActivity.java
@@ -1,22 +1,43 @@
 package com.a2nram.scribba;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setToolbar(); //implementa toolbar
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
     //implementa toolbar
     private void setToolbar(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home); //habilita icono en la barra
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //hace visible el ícono
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //abrir el menú lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
